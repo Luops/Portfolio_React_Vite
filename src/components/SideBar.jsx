7c88f5686a2ec92ce38576a8c86cbc892a03934e
fabricio rios <you@example.com>
2022-07-29 10:50:06 -0300

    Foi criado a base do MainContent, e já editando o lado com os dados (aside).

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,13 @@
 import Avatar from '../img/foto.jpg';
 
 import '../styles/components/sidebar.sass';
+import SocialNetworks from './SocialNetworks';
 
 const SideBar = () => {
     return <aside id="sidebar">
             <img src={Avatar} alt="Fabrício Lopes" />
-            <p className="titles">Desenvolvedor Front-End</p>
-            <p>redes sociais</p>
+            <p className="title">Desenvolvedor Front-End</p>
+            <SocialNetworks />
             <p>informações de contato</p>
             <a href="" className="btn">Download currículo</a>
         </aside>
